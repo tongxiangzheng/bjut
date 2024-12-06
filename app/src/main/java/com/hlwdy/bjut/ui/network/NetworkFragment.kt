514cae9760fb4e909d6b94a03dbcc6f989b52f94
TXZ <920504276@qq.com>
2024-12-06 23:45:41 +0800

    检查dns

--- app/src/main/java/com/hlwdy/bjut/ui/network/NetworkFragment.kt
+++ app/src/main/java/com/hlwdy/bjut/ui/network/NetworkFragment.kt
@@ -1,19 +1,75 @@
 package com.hlwdy.bjut.ui.network
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.CookieManager
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import android.net.DnsResolver
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
 
 import com.hlwdy.bjut.databinding.FragmentNetworkBinding
+import okhttp3.Dns
+import java.net.InetAddress
+import java.util.concurrent.Executors
+
+fun isInternalIp(ip: String): Boolean {
+    val privateIpPatterns = listOf(
+        "^10\\..*".toRegex(),
+        "^172\\.(1[6-9]|2[0-9]|3[0-1])\\..*".toRegex(),
+        "^192\\.168\\..*".toRegex()
+    )
+    return privateIpPatterns.any { it.matches(ip) }
+}
+@RequiresApi(Build.VERSION_CODES.Q)
+fun checkDnsRecords(domain: String) {
+    try {
+        val executor = Executors.newSingleThreadExecutor()
+        val resolver = DnsResolver.getInstance()
+        resolver.query(
+            null,
+            domain,
+            DnsResolver.TYPE_A,
+            DnsResolver.FLAG_NO_CACHE_LOOKUP,
+            executor,
+            null,
+            object : DnsResolver.Callback<List<InetAddress>> {
+                override fun onAnswer(
+                    addresses: List<InetAddress>,
+                    rCode: Int,
+                ) {
+                    if (addresses.isEmpty()){
+                        Log.w("Error","check bjut dns record with no result")
+                    }
+                    for(address in addresses){
+                        val ipAddress = address.hostAddress
+                        Log.d("normal","Host Address: $ipAddress")
+                        val res=isInternalIp(ipAddress)
+                        if (res) {
+                            Log.d("normal","in bjut")
+                        }else{
+                            Log.d("normal","NOT in bjut")
+                        }
+                    }
+                }
+
+                override fun onError(e: DnsResolver.DnsException) {
+                    //错误处理
+                }
+            },
+        )
+
+    } catch (e: Exception) {
+        Log.w("Error","message: ${e.message}")
+    }
+}
+
+
 
 class NetworkFragment : Fragment() {
 
@@ -33,12 +89,13 @@ class NetworkFragment : Fragment() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
+        checkDnsRecords("bjut.edu.cn")
         _binding = FragmentNetworkBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
