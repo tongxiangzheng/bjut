ed3ff1d2e3da3e6c037951ec8d82446f3bcda6b5
TXZ <920504276@qq.com>
2024-12-08 16:22:22 +0800

    调整代码，调整页面

--- app/src/main/java/com/hlwdy/bjut/ui/network/NetworkFragment.kt
+++ app/src/main/java/com/hlwdy/bjut/ui/network/NetworkFragment.kt
@@ -7,8 +7,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import android.os.Handler
 import android.os.Looper
+import android.text.InputType
 import android.util.Log
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.hlwdy.bjut.account_session_util
 
 import java.net.HttpURLConnection
 import java.net.URL
@@ -20,7 +27,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.net.InetAddress
 import org.json.JSONObject
-
+import com.hlwdy.bjut.network_account_util
 
 fun isInternalIp(ip: String): Boolean {
     val privateIpPatterns = listOf(
@@ -35,15 +42,15 @@ fun checkWebsiteAccessibility(urlString: String): Boolean {
         val url = URL(urlString)
         val connection = url.openConnection() as HttpURLConnection
         connection.requestMethod = "GET"
-        connection.connectTimeout=1000
-        connection.readTimeout=1000
+        connection.connectTimeout=500
+        connection.readTimeout=500
         connection.instanceFollowRedirects=false
         connection.connect()
 
         val responseCode = connection.responseCode
         if(responseCode == HttpURLConnection.HTTP_OK){
             val response = connection.inputStream.bufferedReader().use { it.readText() }
-            Log.d("normal","response: "+response)
+            //Log.d("normal","response: "+response)
             response.isNotEmpty()
         }else{
             connection.disconnect()
@@ -54,7 +61,7 @@ fun checkWebsiteAccessibility(urlString: String): Boolean {
     }
 }
 
-fun getJsonFromUrl(urlString: String): String? {
+fun getDataFromUrl(urlString: String): String? {
     try {
         val url = URL(urlString)
         val connection = url.openConnection() as HttpURLConnection
@@ -73,9 +80,58 @@ fun getJsonFromUrl(urlString: String): String? {
         e.printStackTrace()
         return null
     }
-
 }
-
+private fun checkBjutLocation():String{
+    //测试是否为bjut_wifi
+    val wres= checkWebsiteAccessibility("http://wlgn.bjut.edu.cn")
+    if(wres){
+        return "wifi"
+    }
+    //测试是否为宿舍光猫
+    val bres= checkWebsiteAccessibility("http://10.21.221.98")
+    if(bres){
+        return "dorm"
+    }
+    //测试是否为其他情况（例如有线网）
+    val res= checkWebsiteAccessibility("http://lgn.bjut.edu.cn")
+    if(res){
+        return "bjut"
+    }
+    return "unknownType"
+}
+fun checkLoginStatusBjut():Boolean {
+    val urlString="https://lgn.bjut.edu.cn"
+    val htmlData=getDataFromUrl(urlString)?:return false
+    Log.d("normal", "html: $htmlData")
+    return htmlData.contains("<!--Dr.COMWebLoginID_1.htm-->")
+}
+fun checkLoginStatusDormOrWifi(networkStates:String):Boolean {
+    val urlString=when(networkStates){
+        "wifi"->"https://wlgn.bjut.edu.cn/drcom/chkstatus?callback=dr1002"
+        "dorm"->"http://10.21.221.98:801/eportal/portal/online_list"
+        else->""
+    }
+    val resString = getDataFromUrl(urlString) ?: return false
+    try {
+        val jsonString=resString.substringAfter("(").substringBeforeLast(")")
+        if (jsonString != "") {
+            val jsonObject = JSONObject(jsonString)
+            return (jsonObject.getInt("result") == 1)
+        } else {
+            return false
+        }
+    }catch (e:Exception){
+        return false
+    }
+}
+fun checkLoginStatus(networkStates:String):Boolean {
+    return when(networkStates){
+        "wifi"->checkLoginStatusDormOrWifi(networkStates)
+        "dorm"->checkLoginStatusDormOrWifi(networkStates)
+        "bjut"->checkLoginStatusBjut()
+        else->false
+    }
+}
 
 
 class NetworkFragment : Fragment() {
@@ -87,14 +143,24 @@ class NetworkFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private fun showToast(message: String) {
+        activity?.let { fragmentActivity ->
+            Handler(Looper.getMainLooper()).post {
+                if (isAdded) {
+                    Toast.makeText(fragmentActivity, message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
     private fun updateShow(){
         when(networkStates){
             "unknown"->binding.networkStateMonitor.setText("网络状态未知")
             "wifi"->binding.networkStateMonitor.setText("bjut_wifi")
             "dorm"->binding.networkStateMonitor.setText("宿舍光猫")
-            "bjut"->binding.networkStateMonitor.setText("有线网（你是怎么做到的？）")
-            "outBJUT"->binding.networkStateMonitor.setText("校外")
+            "bjut"->binding.networkStateMonitor.setText("有线网")
             "unknownType"->binding.networkStateMonitor.setText("未知校内网络")
+            "outBJUT"->binding.networkStateMonitor.setText("校外")
+            "noNetwork"->binding.networkStateMonitor.setText("无网络")
             else->binding.networkStateMonitor.setText("error: $networkStates")
         }
         if(networkLoginStates){
@@ -103,68 +169,16 @@ class NetworkFragment : Fragment() {
             binding.networkLoginStateMonitor.setText("未登录")
         }
     }
-    private fun checkBjutLocation(){
-        //测试是否为bjut_wifi
-        val wres= checkWebsiteAccessibility("http://wlgn.bjut.edu.cn")
-        if(wres){
-            networkStates="wifi"
-            return
-        }
-        //测试是否为宿舍光猫
-        val bres= checkWebsiteAccessibility("http://10.21.221.98")
-        if(bres){
-            networkStates="dorm"
-            return
-        }
-        //测试是否为其他情况（例如有线网）
-        val res= checkWebsiteAccessibility("http://lgn.bjut.edu.cn")
-        if(res){
-            networkStates="bjut"
-            return
-        }
-        networkStates="unknownType"
-    }
-    private fun checkLoginStatus() {
-        //val host = getServiceHost()
-        //val timestamp = Instant.now().epochSecond
-        //val urlString = "http://${host}/drcom/chkstatus?callback=dr${timestamp}"123"
-        val urlString=when(networkStates){
-            "wifi"->"https://wlgn.bjut.edu.cn/drcom/chkstatus?callback=dr1002"
-            "dorm"->"http://10.21.221.98:801/eportal/portal/online_list"
-            "bjut"->"lgn.bjut.edu.cn"
-            else->""
-        }
-        if(urlString==""){
-            networkLoginStates=false
-            return
-        }
-        val resString = getJsonFromUrl(urlString)
-        if(resString == null){
-            networkLoginStates=false
-            return
-        }
-        try {
-            val jsonString=resString.substringAfter("(").substringBeforeLast(")")
-            if (jsonString != "") {
-                val jsonObject = JSONObject(jsonString)
-                networkLoginStates = (jsonObject.getInt("result") == 1)
-            } else {
-                networkLoginStates = false
-            }
-        }catch (e:Exception){
-            networkLoginStates = false
-        }
 
-    }
+
     private fun checkNetworkStates() {
-        val domain="bjut.edu.cn"
         lifecycleScope.launch {
             val success = withContext(Dispatchers.IO) {
                 try {
-                    val address = InetAddress.getByName(domain)
+                    val address = InetAddress.getByName("bjut.edu.cn")
                     if (isInternalIp(address.hostAddress!!)) {
-                        checkBjutLocation()
-                        checkLoginStatus()
+                        networkStates=checkBjutLocation()
+                        networkLoginStates=checkLoginStatus(networkStates)
                     }else {
                         networkStates = "outBJUT"
                         networkLoginStates = false
@@ -174,10 +188,69 @@ class NetworkFragment : Fragment() {
                     false
                 }
             }
-            if(success){
-                updateShow()
+            if(!success) {
+                networkStates = "noNetwork"
+                networkLoginStates = false
             }
+            updateShow()
         }
+    }
+    private fun setPassword(networkAccount: network_account_util){
+        val builder = MaterialAlertDialogBuilder(requireContext())
+        builder.setTitle("保存密码")
+        val layout = LinearLayout(context).apply {
+            orientation = LinearLayout.VERTICAL
+            setPadding(32, 32, 32, 32)
+        }
+        val usernameLayout = TextInputLayout(requireContext()).apply {
+            layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            hint = "账号"
+            boxBackgroundMode = TextInputLayout.BOX_BACKGROUND_OUTLINE // 设置外边框样式
+        }
+        val username = TextInputEditText(requireContext()).apply { setText(account_session_util(requireContext()).getUserDetails()[account_session_util.KEY_USERNAME]) }
+        usernameLayout.addView(username)
+
+        val passwordLayout = TextInputLayout(requireContext()).apply {
+            layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            hint = "密码"
+            boxBackgroundMode = TextInputLayout.BOX_BACKGROUND_OUTLINE
+            // 添加密码可见性开关
+            endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
+        }
+        val password = TextInputEditText(requireContext()).apply {
+            inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+        }
+        passwordLayout.addView(password)
+
+        layout.addView(usernameLayout)
+        layout.addView(passwordLayout)
+        builder.setView(layout)
+        builder.setPositiveButton("保存") { dialog, _ ->
+            val userAccount = username.text.toString()
+            val userPassword = password.text.toString()
+            networkAccount.editUserData(userAccount, userPassword)
+            showToast("保存成功")
+            dialog.cancel()
+        }
+
+        builder.setNegativeButton("取消") { dialog, _ ->
+            dialog.cancel()
+        }
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+    private fun loginNetwork(networkAccount: network_account_util){
+        if(!networkAccount.haveData()){
+            setPassword(networkAccount)
+        }
+
     }
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -185,14 +258,18 @@ class NetworkFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentNetworkBinding.inflate(inflater, container, false)
+
+        val networkAccount=network_account_util(requireContext())
         checkNetworkStates()
         val root: View = binding.root
-        binding.btnLogin.setOnClickListener {
-
+        binding.btnRefresh.setOnClickListener {
+            checkNetworkStates()
         }
-
+        binding.btnLogin.setOnClickListener {
+            loginNetwork(networkAccount)
+        }
         binding.btnSetPwd.setOnClickListener {
-
+            setPassword(networkAccount)
         }
 
         return root
