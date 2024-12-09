b61982bd86aeed45a87c03ea0fc1b1ca25047132
TXZ <920504276@qq.com>
2024-12-09 21:51:30 +0800

    实现有线网登录，bjut_wifiipv6额外登录

--- app/src/main/java/com/hlwdy/bjut/ui/network/NetworkFragment.kt
+++ app/src/main/java/com/hlwdy/bjut/ui/network/NetworkFragment.kt
@@ -28,6 +28,8 @@ import kotlinx.coroutines.withContext
 import java.net.InetAddress
 import org.json.JSONObject
 import com.hlwdy.bjut.network_account_util
+import java.io.DataOutputStream
+import java.util.Locale
 
 fun isInternalIp(ip: String): Boolean {
     val privateIpPatterns = listOf(
@@ -81,11 +83,20 @@ fun getDataFromUrl(urlString: String): String? {
         return null
     }
 }
-fun postUrl(urlString: String): String? {
+fun postUrl(urlString: String, params: Set<String>): String? {
     try {
         val url = URL(urlString)
         val connection = url.openConnection() as HttpURLConnection
         connection.requestMethod = "POST"
+        connection.doOutput = true
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+        val postData = params.joinToString("&")
+
+        // 发送请求参数
+        val outputStream = DataOutputStream(connection.outputStream)
+        outputStream.writeBytes(postData)
+        outputStream.flush()
+        outputStream.close()
         if (connection.responseCode == HttpURLConnection.HTTP_OK) {
             val inputStream = connection.inputStream
             inputStream.bufferedReader().use { reader ->
@@ -119,39 +130,58 @@ private fun checkBjutLocation():String{
     }
     return "unknownType"
 }
-fun checkLoginStatusBjut():Boolean {
+fun checkLoginStatusBjut():Pair<Boolean,Boolean> {
     val urlString="https://lgn.bjut.edu.cn"
-    val htmlData=getDataFromUrl(urlString)?:return false
-    return htmlData.contains("<!--Dr.COMWebLoginID_1.htm-->")
+    val htmlData=getDataFromUrl(urlString)?:return Pair(false,false)
+    val ipv4states=htmlData.contains("<!--Dr.COMWebLoginID_1.htm-->")
+    val ipv6states=!htmlData.contains(";v6ip='::'")
+    return Pair(ipv4states,(ipv4states&&ipv6states))
 }
-fun checkLoginStatusDormOrWifi(networkStates:String):Boolean {
-    val urlString=when(networkStates){
-        "wifi"->"https://wlgn.bjut.edu.cn/drcom/chkstatus?callback=dr1002"
-        "dorm"->"http://10.21.221.98:801/eportal/portal/online_list"
-        else->""
-    }
-    val resString = getDataFromUrl(urlString) ?: return false
+fun checkLoginStatusDorm():Pair<Boolean,Boolean> {
+    val urlString="http://10.21.221.98:801/eportal/portal/online_list"
+    val resString = getDataFromUrl(urlString) ?: return Pair(false,false)
     try {
         val jsonString=resString.substringAfter("(").substringBeforeLast(")")
         if (jsonString != "") {
             val jsonObject = JSONObject(jsonString)
-            return (jsonObject.getInt("result") == 1)
+            val res=(jsonObject.getInt("result") == 1)
+            return Pair(res,res)
         } else {
-            return false
+            return Pair(false,false)
         }
     }catch (e:Exception){
-        return false
+        return Pair(false,false)
     }
 }
-fun checkLoginStatus(networkStates:String):Boolean {
+fun checkLoginStatusWifi():Pair<Boolean,Boolean> {
+    val urlString="https://wlgn.bjut.edu.cn/drcom/chkstatus?callback=dr1002"
+    val resString = getDataFromUrl(urlString) ?: return Pair(false,false)
+    try {
+        val jsonString=resString.substringAfter("(").substringBeforeLast(")")
+        if (jsonString != "") {
+            val jsonObject = JSONObject(jsonString)
+            val res=(jsonObject.getInt("result") == 1)
+            if(res){
+                return Pair(true,checkLoginStatusBjut().second)
+            }else{
+                return Pair(false,false)
+            }
+        } else {
+            return Pair(false,false)
+        }
+    }catch (e:Exception){
+        return Pair(false,false)
+    }
+}
+fun checkLoginStatus(networkStates:String):Pair<Boolean,Boolean> {
     return when(networkStates){
-        "wifi"->checkLoginStatusDormOrWifi(networkStates)
-        "dorm"->checkLoginStatusDormOrWifi(networkStates)
+        "wifi"->checkLoginStatusWifi()
+        "dorm"->checkLoginStatusDorm()
         "bjut"->checkLoginStatusBjut()
-        else->false
+        else->Pair(false,false)
     }
 }
-fun bjutNetworkLoginDorm(networkAccount: network_account_util):Boolean{
+fun bjutNetworkLoginDorm(networkAccount: network_account_util):Pair<Boolean,Boolean>{
     val user=networkAccount.getUserName()
     val pwd=networkAccount.getUserPwd()
     val urlString="http://10.21.221.98:801/eportal/?c=Portal&a=login&login_method=1&user_account=$user%40campus&user_password=$pwd"
@@ -160,33 +190,80 @@ fun bjutNetworkLoginDorm(networkAccount: network_account_util):Boolean{
     val jsonString=resString.substringAfter("(").substringBeforeLast(")")
     if (jsonString != "") {
         val jsonObject = JSONObject(jsonString)
-        return (jsonObject.getInt("result") == 1)
+        val res=(jsonObject.getInt("result") == 1)
+        return Pair(res,res)
     } else {
-        return false
+        return Pair(false,false)
     }
 }
-fun bjutNetworkLoginWifi(networkAccount: network_account_util):Boolean{
+fun bjutNetworkLoginWifi(networkAccount: network_account_util):Pair<Boolean,Boolean>{
     val user=networkAccount.getUserName()
     val pwd=networkAccount.getUserPwd()
     val urlString="http://wlgn.bjut.edu.cn/drcom/login?callback=dr1002&DDDDD=${user}&upass=${pwd}&0MKKey=123456&R1=0&R2=&R3=0&R6=0&para=00&v6ip=&terminal_type=1&lang=zh%2Dcn&jsVersion=4.1&v=1234&lang=zh"
     val resString=getDataFromUrl(urlString)?:"jsonpReturn({\"result\":0})"
-    Log.d("normal",resString)
+    //Log.d("normal",resString)
     val jsonString=resString.substringAfter("(").substringBeforeLast(")")
     if (jsonString != "") {
         val jsonObject = JSONObject(jsonString)
-        return (jsonObject.getInt("result") == 1)
+        return Pair((jsonObject.getInt("result") == 1),false)
     } else {
-        return false
+        return Pair(false,false)
     }
 }
-fun bjutNetworkLoginBjut(networkAccount: network_account_util):Boolean{
-    return false
+fun bjutNetworkLoginBjut(networkAccount: network_account_util):Pair<Boolean,Boolean>{
+    val user=networkAccount.getUserName()
+    val pwd=networkAccount.getUserPwd()
+    val ipv6address=try{
+        val ipv6Res=postUrl("http://lgn6.bjut.edu.cn/V6?https://lgn.bjut.edu.cn",setOf<String>("DDDDD=${user}","upass=${pwd}","v46s=0","v6ip=","f4serip=172.30.201.10","0MKKey="))
+        //Log.d("normal",ipv6Res?:"empty lgn6 response")
+        ipv6Res?.substringAfter("name='v6ip' value='")?.substringBeforeLast("'>")
+    }catch (e:Exception){
+        null
+    }
+    //Log.d("normal",ipv6address?:"no ipv6 address")
+    val htmlData=ipv6address?.let{
+        postUrl("https://lgn.bjut.edu.cn/",setOf<String>("DDDDD=${user}","upass=${pwd}","0MKKey=Login","v6ip=${it}"))
+    }?:run{
+        postUrl("https://lgn.bjut.edu.cn/",setOf<String>("DDDDD=${user}","upass=${pwd}","v46s=1","0MKKey="))
+    }?:return Pair(false,false)
+    //Log.d("normal",htmlData?:"empty lgn response")
+    return Pair(htmlData.contains("<!--Dr.COMWebLoginID_3.htm-->"),ipv6address!=null)
+}
+
+fun bjutNetworkLoginBjutIpv6(networkAccount: network_account_util):Boolean{
+    val user=networkAccount.getUserName()
+    val pwd=networkAccount.getUserPwd()
+
+    val htmlData=postUrl("https://lgn6.bjut.edu.cn/",setOf<String>("DDDDD=${user}","upass=${pwd}","v46s=2","0MKKey="))?:return false
+    //Log.d("normal",htmlData)
+    return htmlData.contains("<!--Dr.COMWebLoginID_3.htm-->")
+}
+fun checkNetworkFlow():Long{
+    val urlString="https://lgn.bjut.edu.cn"
+    val htmlData=getDataFromUrl(urlString)?:return -1
+    val pattern = Regex("flow='(.*?)'")
+    val matchResult = pattern.find(htmlData)
+    val flowValue = matchResult?.groupValues?.get(1)?.trim()
+    if(flowValue==null){
+        return -1
+    }else{
+        return flowValue.toLongOrNull() ?:-1
+    }
+}
+fun formatFlowSize(sizeInBytes: Long): String {
+    val unit = 1024
+    if (sizeInBytes < unit) return "$sizeInBytes B"
+    val exp = (Math.log(sizeInBytes.toDouble()) / Math.log(unit.toDouble())).toInt()
+    val pre = "KMGTPE"[exp - 1] + "i"
+    return String.format(Locale("zh", "CN"),"%.1f %sB", sizeInBytes / Math.pow(unit.toDouble(), exp.toDouble()), pre)
 }
 class NetworkFragment : Fragment() {
 
     private var _binding: FragmentNetworkBinding? = null
     private var networkStates="unknown"
-    private var networkLoginStates=false
+    private var networkLoginipv4States=false
+    private var networkLoginipv6States=false
+    private var networkFlow=-1L
     private var lastRequireLoginTime=0L
     val handler = Handler(Looper.getMainLooper())
     // This property is only valid between onCreateView and
@@ -212,10 +289,25 @@ class NetworkFragment : Fragment() {
             "noNetwork"->binding.networkStateMonitor.setText("无网络")
             else->binding.networkStateMonitor.setText("error: $networkStates")
         }
-        if(networkLoginStates){
-            binding.networkLoginStateMonitor.setText("已登录")
+        if(networkStates=="wifi"&&networkLoginipv4States){
+            binding.btnwlgnipv6Login.visibility= View.VISIBLE
+        }else{
+            binding.btnwlgnipv6Login.visibility= View.INVISIBLE
+        }
+        if(networkLoginipv4States&&networkLoginipv6States){
+            binding.networkLoginStateMonitor.setText("登录(v4&v6)")
+        }else if(networkLoginipv4States){
+            binding.networkLoginStateMonitor.setText("登录(ipv4)")
+        }else if(networkLoginipv6States){
+            binding.networkLoginStateMonitor.setText("登录(ipv6)")
         }else{
             binding.networkLoginStateMonitor.setText("未登录")
+        }
+        if(networkFlow!=-1L){
+            val text=formatFlowSize( networkFlow*1024)
+            binding.networkFlowMonitor.setText(text)
+        }else{
+            binding.networkFlowMonitor.setText("未知")
         }
     }
 
@@ -227,16 +319,24 @@ class NetworkFragment : Fragment() {
                     val address = InetAddress.getByName("bjut.edu.cn")
                     if (isInternalIp(address.hostAddress!!)) {
                         networkStates=checkBjutLocation()
-                        networkLoginStates=checkLoginStatus(networkStates)
-                        if(lastRequireLoginTime!=0L){
-                            if(System.currentTimeMillis()-lastRequireLoginTime<5000){
-                                loginNetwork(networkAccount)
+                        val pair=checkLoginStatus(networkStates)
+                        networkLoginipv4States=pair.first
+                        networkLoginipv6States=pair.second
+                        if(networkLoginipv4States) {
+                            networkFlow=checkNetworkFlow()
+                        }else{
+                            if (lastRequireLoginTime != 0L) {
+                                if (System.currentTimeMillis() - lastRequireLoginTime < 5000) {
+                                    loginNetwork(networkAccount)
+                                }
+                                lastRequireLoginTime = 0L
                             }
-                            lastRequireLoginTime=0L
+
                         }
                     }else {
                         networkStates = "outBJUT"
-                        networkLoginStates = false
+                        networkLoginipv4States=false
+                        networkLoginipv6States=false
                     }
                     true
                 } catch (e: Exception) {
@@ -245,7 +345,8 @@ class NetworkFragment : Fragment() {
             }
             if(!success) {
                 networkStates = "noNetwork"
-                networkLoginStates = false
+                networkLoginipv4States=false
+                networkLoginipv6States=false
             }
             updateShow()
         }
@@ -302,7 +403,7 @@ class NetworkFragment : Fragment() {
         dialog.show()
     }
     private fun loginNetwork(networkAccount: network_account_util){
-        if(networkLoginStates==true){
+        if(networkLoginipv4States==true){
             showToast("已登录")
             return
         }
@@ -311,27 +412,54 @@ class NetworkFragment : Fragment() {
         }
         if(networkStates=="dorm"){
             lifecycleScope.launch {
-                networkLoginStates=withContext(Dispatchers.IO) {
+                val pair=withContext(Dispatchers.IO) {
                     bjutNetworkLoginDorm(networkAccount)
                 }
+                networkLoginipv4States=pair.first
+                networkLoginipv6States=pair.second
                 updateShow()
+                checkNetworkFlow()
             }
         }else if(networkStates=="wifi"){
             lifecycleScope.launch {
-                networkLoginStates=withContext(Dispatchers.IO) {
+                val pair=withContext(Dispatchers.IO) {
                     bjutNetworkLoginWifi(networkAccount)
                 }
+                networkLoginipv4States=pair.first
+                networkLoginipv6States=pair.second
                 updateShow()
+                checkNetworkFlow()
             }
         }else if(networkStates=="bjut"){
             lifecycleScope.launch {
-                networkLoginStates=withContext(Dispatchers.IO) {
+                val pair=withContext(Dispatchers.IO) {
                     bjutNetworkLoginBjut(networkAccount)
                 }
+                networkLoginipv4States=pair.first
+                networkLoginipv6States=pair.second
                 updateShow()
+                checkNetworkFlow()
             }
         }else{
             lastRequireLoginTime=System.currentTimeMillis()
+        }
+    }
+    private fun loginNetworkWifiIpv6(networkAccount: network_account_util){
+        if(networkLoginipv6States==true){
+            showToast("已登录")
+            return
+        }
+        if(!networkAccount.haveData()){
+            setPassword(networkAccount)
+        }
+        if(networkStates!="wifi") {
+            return
+        }
+        lifecycleScope.launch {
+            networkLoginipv6States=withContext(Dispatchers.IO) {
+                bjutNetworkLoginBjutIpv6(networkAccount)
+            }
+            updateShow()
         }
     }
     override fun onCreateView(
@@ -353,7 +481,10 @@ class NetworkFragment : Fragment() {
         binding.btnSetPwd.setOnClickListener {
             setPassword(networkAccount)
         }
-
+        binding.btnwlgnipv6Login.visibility= View.INVISIBLE
+        binding.btnwlgnipv6Login.setOnClickListener{
+            loginNetworkWifiIpv6(networkAccount)
+        }
         return root
     }
 
